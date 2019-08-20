d9ce0ebdc41ccf8ae95922cfc61d0476f8c44b66
Mandatorytoast <optionaltoast420@gmail.com>
2019-08-20 20:18:16 +0100

    working version

--- main.go
+++ main.go
@@ -15,30 +15,33 @@ func check(e error){
         }
 }
 
+
 func main() {
         file, err := ioutil.ReadFile("./test")
         check(err)
-        p := Pointer{}
+        p := Pointer{15000}
         nodes := make([]int, 30000)
         jmp_index := 0
-        for index, value := range file {
-                if value == 62 {
+        index := 0
+        for index < len(file) {
+                if file[index] == 62 {
                         p.Position += 1
-                } else if value == 60{
+                } else if file[index] == 60{
                         p.Position -= 1
-                } else if value == 43{
+                } else if file[index] == 43{
                         nodes[p.Position] += 1
-                } else if value == 45{
+                } else if file[index] == 45{
                         nodes[p.Position] -= 1
-                } else if value == 91{
-                        jmp_index =  index
-                        fmt.Printf("Jmp_index: %d\n", jmp_index)
-                } else if value == 93{
-                        fmt.Printf("node value %d\n", nodes[p.Position])
+                } else if file[index] == 91{
+                        jmp_index = index
+                } else if file[index] == 46{
+                        fmt.Printf("%c", nodes[p.Position])
+                } else if file[index] == 93{
                         if nodes[p.Position] != 0{
-                                p.Position = jmp_index
-                                fmt.Printf("Looped back")
+                                index = jmp_index
+                                continue
                         }
                 }
+                index += 1
         }
 }
